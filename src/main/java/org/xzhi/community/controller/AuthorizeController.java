0e3d9ea276c7e0370cbbdf91290b7af0abf03060
Black <xzhi8861890@163.com>
2019-09-20 15:19:36 +0800

    update application

--- src/main/java/org/xzhi/community/controller/AuthorizeController.java
+++ src/main/java/org/xzhi/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package org.xzhi.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,24 @@ public class AuthorizeController {
         @Autowired
         private GithubProvider githubProvider;
 
+        @Value("${github.client.id}")
+        private String clientId;
+
+        @Value("${github.client.sercet}")
+        private String clientSercet;
+
+        @Value("${github.redirect.uri}")
+        private String redirectUri;
+
+
         @GetMapping("callback")
         public String callback(@RequestParam(name="code") String code,
                                @RequestParam(name="state") String state){
             AccessTokenDto accessTokenDto = new AccessTokenDto();
-            accessTokenDto.setClient_id("bd74c0284c60027fe607");
-            accessTokenDto.setClient_secret("ebc8bc98044608af493b78b3c32b722a7a9407e6");
+            accessTokenDto.setClient_id(clientId);
+            accessTokenDto.setClient_secret(clientSercet);
             accessTokenDto.setCode(code);
-            accessTokenDto.setRedirect_uri("http://localhost:8887/callback");
+            accessTokenDto.setRedirect_uri(redirectUri);
             accessTokenDto.setState(state);
             String accessToken = githubProvider.getAccessToken(accessTokenDto);
             githubProvider.getAccessToken(new AccessTokenDto());

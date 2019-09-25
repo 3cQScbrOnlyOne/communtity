127e662822fddb96bd0e749d0a6ccb1d37618f20
Black <xzhi8861890@163.com>
2019-09-25 13:35:10 +0800

    mybatis

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 [Spring 文档](https://spring.io/guides)  
 [Spring Web](https://spring.io/guides/gs/serving-web-content/)  
 [BootStrap](https://v3.bootcss.com/getting-started/)  
-[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
-
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
+[Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 
 ####工具
 [Git](https://git-scm.com/downloads)
--- src/main/java/org/xzhi/community/controller/AuthorizeController.java
+++ src/main/java/org/xzhi/community/controller/AuthorizeController.java
@@ -7,37 +7,61 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.xzhi.community.dto.AccessTokenDto;
 import org.xzhi.community.dto.GithubUser;
+import org.xzhi.community.mapper.UserMapper;
+import org.xzhi.community.model.User;
 import org.xzhi.community.provider.GithubProvider;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
 
-        @Autowired
-        private GithubProvider githubProvider;
+    @Autowired
+    private GithubProvider githubProvider;
 
-        @Value("${github.client.id}")
-        private String clientId;
+    @Value("${github.client.id}")
+    private String clientId;
 
-        @Value("${github.client.sercet}")
-        private String clientSercet;
+    @Value("${github.client.sercet}")
+    private String clientSercet;
 
-        @Value("${github.redirect.uri}")
-        private String redirectUri;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+    private GithubUser user;
 
 
-        @GetMapping("callback")
-        public String callback(@RequestParam(name="code") String code,
-                               @RequestParam(name="state") String state){
-            AccessTokenDto accessTokenDto = new AccessTokenDto();
-            accessTokenDto.setClient_id(clientId);
-            accessTokenDto.setClient_secret(clientSercet);
-            accessTokenDto.setCode(code);
-            accessTokenDto.setRedirect_uri(redirectUri);
-            accessTokenDto.setState(state);
-            String accessToken = githubProvider.getAccessToken(accessTokenDto);
-            githubProvider.getAccessToken(new AccessTokenDto());
-            GithubUser user = githubProvider.getUser(accessToken);
-            System.out.println(user.getName());
-            return "index";
+    @GetMapping("callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSercet);
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        githubProvider.getAccessToken(new AccessTokenDto());
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败
+            return "redirect:/";
         }
+
+    }
 }
